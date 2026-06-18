908e29d55073a63ba41318a62baa320c5b7b327a
juli <vitnori39@gmail.com>
2026-06-18 13:20:13 +0700

    perbaiki otp email telah selesai diperbaharui kodenya

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -3,12 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OtpMail extends Mailable
+class OtpMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
